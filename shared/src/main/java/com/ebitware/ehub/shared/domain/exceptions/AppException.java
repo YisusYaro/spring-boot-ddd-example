7210ec52fba67d364e9cfda6449db90b5461deb8
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2023-04-14 13:55:39 -0600

    feat: add app exception

---
+++ shared/src/main/java/com/ebitware/ehub/shared/domain/exceptions/AppException.java
@@ -0,0 +1,7 @@
+package com.ebitware.ehub.shared.domain.exceptions;
+
+public class AppException extends RuntimeException {
+    public AppException(String message) {
+        super(message);
+    }
+}
