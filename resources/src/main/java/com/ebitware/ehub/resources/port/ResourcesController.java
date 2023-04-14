d1508a94cd8b9a70f4d75ec6cbd33f7667912aa3
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2023-04-14 14:04:40 -0600

    feat(resources): add create resource endpoint

--- resources/src/main/java/com/ebitware/ehub/resources/port/ResourcesController.java
+++ resources/src/main/java/com/ebitware/ehub/resources/port/ResourcesController.java
@@ -1,22 +1,39 @@
 package com.ebitware.ehub.resources.port;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ebitware.ehub.resources.application.commands.CreateResourceCommand;
+import com.ebitware.ehub.resources.application.commands.CreateResourceHandler;
+import com.ebitware.ehub.resources.port.requests.CreateResourceRequestBody;
 import com.ebitware.ehub.shared.domain.EHubLogger;
+import com.ebitware.ehub.shared.domain.IdGenerator;
+import com.ebitware.ehub.shared.port.Responser;
 
 @RestController
 @RequestMapping("/resources")
 public class ResourcesController {
     @Autowired
     private EHubLogger eHubLogger;
+    @Autowired
+    private IdGenerator idGenerator;
+    @Autowired
+    private Responser responser;
+    @Autowired
+    private CreateResourceHandler createResourceHandler;
 
-    @GetMapping
-    public String getResources() {
-        String message = "Server is working ✨";
-        this.eHubLogger.log(message);
-        return message;
+    @PostMapping
+    public ResponseEntity<Object> createResource(@RequestBody CreateResourceRequestBody body) {
+        String operationId = this.idGenerator.get();
+        return this.responser.handleCommand(operationId, () -> {
+            this.eHubLogger.log("Create resource operation id: " + operationId);
+            CreateResourceCommand command = new CreateResourceCommand(operationId, body.getId());
+            return this.createResourceHandler.handle(command);
+        });
     }
+
 }
---
+++ resources/src/main/java/com/ebitware/ehub/resources/port/requests/CreateResourceRequestBody.java
@@ -0,0 +1,13 @@
+package com.ebitware.ehub.resources.port.requests;
+
+public class CreateResourceRequestBody {
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
