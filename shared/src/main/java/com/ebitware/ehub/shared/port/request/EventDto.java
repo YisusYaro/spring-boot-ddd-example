19a94b40db42f6b50220c624887fddbd558df646
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2023-05-15 23:50:45 -0600

    feat: add event dto

---
+++ shared/src/main/java/com/ebitware/ehub/shared/port/request/EventDto.java
@@ -0,0 +1,41 @@
+package com.ebitware.ehub.shared.port.request;
+
+public class EventDto {
+    private String aggregateId;
+    private String eventId;
+    private String occurredOn;
+    private String eventName;
+
+    public String getAggregateId() {
+        return aggregateId;
+    }
+
+    public void setAggregateId(String aggregateId) {
+        this.aggregateId = aggregateId;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getOccurredOn() {
+        return occurredOn;
+    }
+
+    public void setOccurredOn(String occurredOn) {
+        this.occurredOn = occurredOn;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+}
