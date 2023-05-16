d62fee7663b4f645e7a9e11acdd958553871476c
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2023-05-15 23:52:49 -0600

    feat(resources): enable rabbit

--- resources/src/main/java/com/ebitware/ehub/resources/ResourcesApplication.java
+++ resources/src/main/java/com/ebitware/ehub/resources/ResourcesApplication.java
@@ -1,8 +1,10 @@
 package com.ebitware.ehub.resources;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication(scanBasePackages = "com.ebitware.ehub")
 public class ResourcesApplication {
 
